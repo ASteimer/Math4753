dede1bf0c4afa3fd30a6b6b302ac72bdd27e15b9
ASteimer <alexander.d.steimer-1@Ou.edu>
2023-07-20 03:58:23 -0500

    Set up the Package and added the myfun function

---
+++ R/myfun.R
@@ -0,0 +1,12 @@
+#' @title My First Function
+#'
+#' @param x A Quantitative Vector
+#'
+#' @return A Quantitative Vector, the Square of the input
+#' @export
+#'
+#' @examples
+#' myfun(x = 1:10)
+myfun<-function(x){
+  x^2
+}
